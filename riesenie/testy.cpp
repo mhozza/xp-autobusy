2147dc40cc2a352ea95531c89680db56f61e7d01
Tatiana Tothova <tatiana.toth@gmail.com>
2013-04-19 23:19:15 +0200

    lod2->lod

--- riesenie/testy.cpp
+++ riesenie/testy.cpp
@@ -20,13 +20,13 @@ void testyDopravnychProstriedkov(){
   testEqual("kocik", dp.vratDopravnyProstriedok(obrazky::kocik),"");
   testEqual("raketa", dp.vratDopravnyProstriedok(obrazky::raketa),"");
   testEqual("autobus", dp.vratDopravnyProstriedok(obrazky::autobus),"");
-  testEqual("autobus2", dp.vratDopravnyProstriedok(obrazky::autobus2),"");
+  testEqual("autobus", dp.vratDopravnyProstriedok(obrazky::autobus2),"");
   testEqual("trolejbus", dp.vratDopravnyProstriedok(obrazky::trolejbus),"");
   testEqual("elektricka", dp.vratDopravnyProstriedok(obrazky::elektricka),"");
   testEqual("bicykel", dp.vratDopravnyProstriedok(obrazky::bicykel),"");
   testEqual("korcula", dp.vratDopravnyProstriedok(obrazky::korcula),"");
   testEqual("lod", dp.vratDopravnyProstriedok(obrazky::lod),"");
-  testEqual("lod2", dp.vratDopravnyProstriedok(obrazky::lod2),"");
+  testEqual("lod", dp.vratDopravnyProstriedok(obrazky::lod2),"");
   testEqual("ponorka", dp.vratDopravnyProstriedok(obrazky::ponorka),"");
   testEqual("vzducholod", dp.vratDopravnyProstriedok(obrazky::vzducholod),"");
   testEqual("skateboard", dp.vratDopravnyProstriedok(obrazky::skateboard),"");
